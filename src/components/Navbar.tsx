db0dfdae770cc5219841286c23a139ce43ec8a52
Atakan Turgut <atakan.turg@gmail.com>
2026-04-21 22:59:23 -0400

    Decrease Navbar logo size slightly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,7 +58,7 @@ export function Navbar() {
             onClick={() => setIsOpen(false)}
             className="px-4 hover:opacity-80 transition-opacity flex items-center justify-center -my-1"
           >
-            <img src="/mve-logo-transparent.png" alt="MVE Logo" className="h-10 md:h-11 w-auto object-contain scale-[1.15]" />
+            <img src="/mve-logo-transparent.png" alt="MVE Logo" className="h-8 w-auto object-contain scale-[1.1]" />
           </Link>
 
           {/* Contact CTA */}
